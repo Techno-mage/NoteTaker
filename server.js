26b6a311ef4eb6af2f100f91d91d3145a7a23278
Erik Schaal <schaa077@umn.edu>
2020-07-09 21:50:34 -0500

    got var notes to read from db.json

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 
 var app = express();
 var PORT = process.env.PORT || 6660;
+
+var notes;
+
+function loadFile(){
+    fs.readFile(__dirname+ "/db/db.json", "utf-8", (err, data) => {
+        if (err) {
+            return err;
+        }else{
+            console.log(data) 
+            return data;
+        }
+    });
+}
+
+
+
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
@@ -12,7 +29,13 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
+app.get("/api/notes", function(req, res) {
+    return res.json(notes)
+
+});
+
 
 app.listen(PORT, function () {
+    notes =loadFile();
     console.log("App listening on PORT " + PORT);
 });
