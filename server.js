ea0ac389110e35a8592961a68d8e5bcac4a5d570
Erik Schaal <schaa077@umn.edu>
2020-07-13 11:33:32 -0500

    notehandler can add new notes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
+const { networkInterfaces } = require("os");
 
 var app = express();
 app.use(express.static('public'));
@@ -9,8 +10,10 @@ app.use(express.json());
 
 var PORT = process.env.PORT || 6660;
 
-
-
+//Todo Add an id element
+//todo add a create method
+//todo add a delete method
+/*
 function loadFile(){
     return fs.readFileSync(__dirname+ "/db/db.json", "utf-8", (err, data) => {
         if (err) {
@@ -26,6 +29,34 @@ function loadFile(){
 
 var notes = loadFile();
 console.log(notes)
+*/
+var noteHandler = {
+    notes:[],
+
+    loadNotes(){
+        var output = fs.readFileSync(__dirname+ "/db/db.json", "utf-8", (err, data) => {
+            if (err) {
+                return err;
+            }else{
+                
+                return data;
+            }
+    
+        });
+        this.notes = JSON.parse(output)
+    },
+
+    newNote(title, text, id){
+        this.notes.push({
+            title:title, text:text, id:id
+        })
+    }
+
+
+}
+
+noteHandler.loadNotes();
+noteHandler.newNote("NewTitle","NewNote","8739")
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
@@ -38,8 +69,8 @@ app.get("/notes", function (req, res) {
 
 app.get("/api/notes", function(req, res) {
     //var notes = loadFile();
-    console.log("sending notes: " + notes)
-    res.json(JSON.parse(notes))
+    console.log("sending notes: " + noteHandler.notes)
+    res.json(noteHandler.notes)
     res.end()
 
 });
